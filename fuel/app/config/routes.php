b734a37323b86863f5d4d7772380f44a14e16a7c
Meritas <shen.lee.alterd@gmail.com>
2014-10-31 14:27:49 +0200

    untracked routing file pushed

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -3,5 +3,9 @@ return array(
 	'_root_'  => 'story/index',  // The default route
 	'_404_'   => '',    // The main 404 route
 	
-	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
+	//'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
+	'register' => 'users/register_character',
+	'login' => 'users/login',
+	'logout' => 'users/logout',
+	'character_sheet' => 'character/sheet/view'
 );
