2d01142a0e1ee7d7e852f4c08aeec85426a80594
Meritas <shen.lee.alterd@gmail.com>
2014-11-11 17:00:49 +0200

    playing with pgsql options

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -5,12 +5,26 @@
 
 return array(
 	'default' => array(
-		'type'           => 'pdo',
 		'connection'  => array(
-			'dsn'        => 'pgsql:host=ec2-54-163-255-191.compute-1.amazonaws.com;dbname=d8qs65cemqupe2',
-			'username'   => 'dncglveqepldhf',
-			'password'   => 'TcAoOVviwh71pXWHo54hoUc8At',
+			'dsn'        => 'mysql:host=localhost;dbname=thegame',
+			'username'   => 'root',
+			'password'   => '123',
 		),
-		'charset'        => 'utf8',
 	),
 );
+
+
+
+
+/*return array(
+	'default' => array(
+	    'type'           => 'pdo',
+    	'charset'        => '',
+    	 'identifier' => '"',
+	    'connection'     => array(
+	        'dsn'            => 'pgsql:host=ec2-54-163-255-191.compute-1.amazonaws.com;dbname=d8qs65cemqupe2',
+	        'username'       => 'dncglveqepldhf',
+	        'password'       => 'TcAoOVviwh71pXWHo54hoUc8At',
+	    ),	    
+	)
+);*/ //pgsql settings
