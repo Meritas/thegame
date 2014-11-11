85be6cfb67fce113c83994daab2e87a782527f3a
Meritas <shen.lee.alterd@gmail.com>
2014-11-11 10:02:29 +0200

    configs

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -7,7 +7,7 @@ return array(
 	'default' => array(
 		'type'           => 'pdo',
 		'connection'  => array(
-			'dsn'        => 'pgsql:host=localhost;dbname=d8qs65cemqupe2',
+			'dsn'        => 'pgsql:host=ec2-54-163-255-191.compute-1.amazonaws.com;dbname=d8qs65cemqupe2',
 			'username'   => 'dncglveqepldhf',
 			'password'   => 'TcAoOVviwh71pXWHo54hoUc8At',
 		),
